9e5a51c9ad77e1c93935790d1597d47f01c232dc
pelegsch666 <peleg.mazuz@gmail.com>
2023-07-06 09:13:20 +0300

    last

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import OpeningPage from './OpeningPage.jsx';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from './App.jsx';
+
 
 const router = createBrowserRouter([
   {
